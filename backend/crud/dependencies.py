457bac0b20e14b8c7c9b7ba71fdd8f16acbdb1e3
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-02-27 11:02:09 +0100

    add CRUD Generators

--- backend/crud/dependencies.py
+++ backend/crud/dependencies.py
@@ -2,6 +2,9 @@ from typing import Generator
 
 from database.config import async_session
 from crud.user import UserCRUD
+from crud.accounts import AccountsCRUD
+from crud.categories import CategoriesCRUD
+from crud.transactions import TransactionCRUD
 
 
 async def get_db() -> Generator:
@@ -14,3 +17,19 @@ async def get_user_crud() -> Generator:
     async with async_session() as session:
         async with session.begin():
             yield UserCRUD(session)
+
+async def get_account_crud() -> Generator:
+    async with async_session() as session:
+        async with session.begin():
+            yield AccountsCRUD(session)
+
+async def get_transaction_crud() -> Generator:
+    async with async_session() as session:
+        async with session.begin():
+            yield TransactionCRUD(session)
+
+async def get_category_crud() -> Generator:
+    async with async_session() as session:
+        async with session.begin():
+            yield CategoriesCRUD(session)
+
