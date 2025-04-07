99218de4c5f9b792c11249619004afcab2e81cf8
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-04-07 20:22:27 +0200

    add Cascade deletion

--- backend/crud/categories.py
+++ backend/crud/categories.py
@@ -4,8 +4,10 @@ from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
-from models.model import CategoriesModels
+from models.model import CategoriesModels, TransactionsModels
 import schemas.categories as categories_schema
+from crud.transactions import TransactionCRUD
+from crud.accounts import AccountsCRUD
 
 class CategoriesCRUD:
     db_session = None
@@ -55,6 +57,20 @@ class CategoriesCRUD:
 
     async def delete_category(self, category_id, username):
         stmt = delete(CategoriesModels).where(CategoriesModels.id == category_id).where(CategoriesModels.username == username)
+        await self.delete_transactions_of_category(category_id, username)
         stmt.execution_options(synchronize_session="fetch")
         await self.db_session.execute(stmt)
+
+    async def delete_transactions_of_category(self, category_id: int, username: str):
+        account_crud = AccountsCRUD(self.db_session)
+        account = await account_crud.get_account_of_user(username=username)
+        print(account)
+        if account is None:
+            return None
+        
+        transaction_crud = TransactionCRUD(self.db_session)
+        transactions = await transaction_crud.get_transactions_by_category_and_account(category_id, account.id)
+
+        for transaction in transactions:
+            await transaction_crud.delete_transaction(transaction.id, account_db=account_crud)
         
